3296043f11f2d614847eddfae8fb4f2e75eca087
Anna <annaczech93@gmail.com>
2019-10-10 10:46:31 +0200

    Frontend init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import CssBaseLine from '@material-ui/core/CssBaseline'
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+        <CssBaseLine/>
+        <Container>
+          <Button variant="contained" color="primary">
+            Sign Up
+          </Button>
+        </Container>
+    </React.Fragment>
   );
 }
 
