a4f98bc96aba041b018e9aea8b93253c30f4b669
Anna <annaczech93@gmail.com>
2019-10-18 15:10:38 +0200

    courses page styled

--- src/pages/courses/courses.jsx
+++ src/pages/courses/courses.jsx
@@ -1,26 +1,91 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import AddIcon from '@material-ui/icons/Add';
+
+const useStyles = makeStyles({
+  card: {
+    minWidth: 275,
+    border: '2px solid lightsteelblue',
+    background: 'rgb(236, 236, 255)',
+    marginTop: '1rem'
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  wrapper: {
+    padding: '2rem'
+  },
+  button: {
+    backgroundColor: 'steelblue',
+    position: 'fixed',
+    right: '2rem',
+    bottom: '30px',
+    zIndex: 99,
+
+  },
+  icon: {
+    color: 'white'
+  }
+});
 export default function Courses() {
 
-  const [courses, setCourses] =  useState([]);
-  useEffect(()=> {
+  const classes = useStyles();
+  const [courses, setCourses] = useState([]);
+  useEffect(() => {
     let fetched = false;
-      async function fetchCourses() {
-          const result = await axios.get('http://localhost:3000/courses/all')
-          if (!fetched) {
-            setCourses(result.data)
-          }
+    async function fetchCourses() {
+      const result = await axios.get('http://localhost:3000/courses/all')
+      if (!fetched) {
+        setCourses(result.data)
       }
-      fetchCourses()
-      return () => { fetched = true; }
-    });
+    }
+    fetchCourses()
+    return () => { fetched = true; }
+  });
   return (
-    <ul>
-    {courses.map((item) => (
-      <li key={item.name}>
-        <a href={item.url}>{item.description}</a>
-      </li>
-    ))}
-  </ul>
+    <div className={classes.wrapper}>
+      {courses.map((item) => (
+        <Card className={classes.card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              {item.description}
+            </Typography>
+            <Typography variant="h5" component="h2">
+              {item.name}
+            </Typography>
+            <Typography color="textSecondary">
+              {item.members}
+            </Typography>
+            <Typography variant="body2" component="p">
+              {item.noOfMembers}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      ))}
+      <IconButton
+        className={classes.button}
+        aria-label="add">
+        <AddIcon className={classes.icon} />
+      </IconButton>
+    </div>
+
   )
 }
